17c7133739ee29ad68913f65f4c99e4edb593ec1
Sp4wn <48498501+537034776e@users.noreply.github.com>
2021-03-09 21:52:58 +0100

    Create SocialNewtork.cs

---
+++ SocialNewtork.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string postText = Console.ReadLine();
+
+            Post post = new Post();
+            post.Text = postText;
+            post.ShowPost();
+
+        }
+    }
+
+    class Post
+    {
+        private string text;
+
+        public Post(){
+            Console.WriteLine("New post");
+        }
+        
+public string Text{
+
+    get{return text;}
+    set{text=value;}
+}        
+
+        public void ShowPost()
+        {
+            Console.WriteLine(text);
+        }
+                
+    }
+}
